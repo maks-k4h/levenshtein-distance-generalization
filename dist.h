31d733387364c2bbb2b275faccf11bcff0bd9aca
Maksym Konevych <konevichdeveloper@gmail.com>
2022-05-15 21:49:25 +0300

    Memory complexity O(n^2) -> O(n)

--- dist.h
+++ dist.h
@@ -5,29 +5,31 @@
 #include <cctype>
 #include <string>
 #include <vector>
+#include <array>
 
 // T must provide .size() and access by index.
 // Elements of arr_ must be comparable by '==' .
 template <typename T>
 size_t editDistance(const T &arr1, const T &arr2) {
-    std::vector<std::vector<size_t>>
-            memo(arr1.size() + 1, std::vector<size_t>(arr2.size() + 1));
-    for (auto i = 0; i < arr1.size() + 1; ++i)
-        memo[i][0] = i;
+    std::array<std::vector<size_t>, 2> memo{
+        std::vector<size_t>(arr2.size() + 1),
+        std::vector<size_t>(arr2.size() + 1)
+    };
     for (auto i = 0; i < arr2.size() + 1; ++i)
         memo[0][i] = i;
 
-    for (auto i = 1; i < arr1.size() + 1; ++i)
-        for (auto j = 1; j < arr2.size() + 1; ++j)
+    for (auto i = 1; i < arr1.size() + 1; ++i) {
+        for (auto j = 1; j < arr2.size() + 1; ++j) {
             if (arr1[i - 1] == arr2[j - 1])
-                memo[i][j] = memo[i - 1][j - 1];
+                memo[1][j] = memo[0][j - 1];
             else
-                memo[i][j] = 1 + std::min({
-                        memo[ i- 1][j],
-                        memo[i][j - 1],
-                        memo[i - 1][j - 1]});
+                memo[1][j] = 1 + std::min(
+                        {memo[0][j], memo[0][j-1], memo[1][j - 1]});
+        }
+        memo[0] = memo[1];
+    }
 
-    return memo[arr1.size()][arr2.size()];
+    return *(memo[1].end() - 1);
 }
 
 #endif //DIST_H_
